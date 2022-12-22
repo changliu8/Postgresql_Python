1c0ba74b4ff8ff43f4e43ef015a06f032fd86da9
cliu <lc928831980@hotmail.com>
2022-12-21 20:41:02 -0700

    connected to the database and retrive information from table 'person'

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-print("123")
-print("234")
-for i in range(1,20):
-    print(i)
+import psycopg2
+
+conn = psycopg2.connect(database="test", user = "cliu", password = "Qq..1234", host = "127.0.0.1", port = "5432")
+
+cur = conn.cursor()
+
+cur.execute("select * from person;")
+rows = cur.fetchall()
+
+'''
+for row in rows:
+   print ("ID = ", row[0])
+   print ("NAME = ", row[1]+" "+row[2])
+   print ("GENDER = ", row[3])
+   print ("date_of_birth = ", row[4])
+   print ("email = ", row[5], "\n")
+'''
+
+print(rows[0])
+
+conn.close()
